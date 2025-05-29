501cb6af5ea7aa71845d7999f1c44bc319a6468f
Abbas Ismael <aismoctar@gmail.com>
2025-05-29 22:51:49 +0100

    Update array_intersection.c

--- array_intersection.c
+++ array_intersection.c
@@ -7,23 +7,6 @@ void recup_valeur(int tab[], int n) {
     }
 }
 
-// Remplit le tableau inter[] avec les éléments communs à tab1 et tab2
-// Retourne le nombre d'éléments dans l'intersection
-int intersection(int tab1[], int tab2[], int n1, int n2, int inter[]) {
-    int k = 0;
-
-    for (int i = 0; i < n1; i++) {
-        for (int j = 0; j < n2; j++) {  // Corrigé ici
-            if (tab1[i] == tab2[j]) {
-                inter[k++] = tab1[i];
-                break;  // Évite les doublons si tab2 contient plusieurs fois la même valeur
-            }
-        }
-    }
-
-    return k;  // Retourne le nombre d’éléments dans l’intersection
-}
-
 int main(void) {
     int n1, n2;
     scanf("%d", &n1);
@@ -34,14 +17,19 @@ int main(void) {
     int tab2[n2];
     recup_valeur(tab2, n2);
 
+
     int inter[n1 < n2 ? n1 : n2];  // Taille maximale possible de l'intersection
-    int taille_inter = intersection(tab1, tab2, n1, n2, inter);
 
     // Affichage du résultat
     printf("Intersection : ");
-    for (int i = 0; i < taille_inter; i++) {
-        printf("%d ", inter[i]);
+    for (int i = 0; i < n1; i++) {
+        for (int j = 0; j < n2; j++) {  // Corrigé ici
+            if (tab1[i] == tab2[j]) {
+                printf("%d ", tab1[i]);
+            }
+        }
     }
+
     printf("\n");
 
     return 0;

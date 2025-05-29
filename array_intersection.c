a822dbf5d5274f14eac4b47233becfd7f2e02744
Abbas Ismael <aismoctar@gmail.com>
2025-05-29 21:13:57 +0100

    Update array_intersection.c

--- array_intersection.c
+++ array_intersection.c
@@ -9,19 +9,16 @@ void recup_valeur(int tab[], int n) {
 
 // Remplit le tableau inter[] avec les éléments communs à tab1 et tab2
 // Retourne le nombre d'éléments dans l'intersection
-int intersection(int tab1[], int tab2[], int n1, int n2, int inter[]) {
+void intersection(int tab1[], int tab2[], int n1, int n2, int inter[]) {
     int k = 0;
 
     for (int i = 0; i < n1; i++) {
         for (int j = 0; j < n2; j++) {  // Corrigé ici
             if (tab1[i] == tab2[j]) {
-                inter[k++] = tab1[i];
-                break;  // Évite les doublons si tab2 contient plusieurs fois la même valeur
+                printf("%d ", tab1[i]);
             }
         }
     }
-
-    return k;  // Retourne le nombre d’éléments dans l’intersection
 }
 
 int main(void) {
@@ -35,12 +32,8 @@ int main(void) {
     recup_valeur(tab2, n2);
 
     int inter[n1 < n2 ? n1 : n2];  // Taille maximale possible de l'intersection
-    int taille_inter = intersection(tab1, tab2, n1, n2, inter);
+    intersection(tab1, tab2, n1, n2, inter);
 
-    // Affichage du résultat
-    for (int i = 0; i < taille_inter; i++) {
-        printf("%d ", inter[i]);
-    }
     printf("\n");
 
     return 0;

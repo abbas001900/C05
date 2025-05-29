68e5e58aa32e84b9924dd608979a53089e8b6c52
Abbas Ismael <aismoctar@gmail.com>
2025-05-29 19:06:27 +0100

    Update rotate_left.c

--- rotate_left.c
+++ rotate_left.c
@@ -1,28 +1,46 @@
 #include <stdio.h>
 
-int main(void) {
-    int n;
-
-    scanf("%d", &n);
-
-    int tab[n];
-
+void recup_valeur(int tab[], int n) {
     for (int i = 0; i < n; i++) {
         scanf("%d", &tab[i]);
     }
+}
 
-    int temp = tab[0];
-
-    for (int i = 0; i < n - 1; i++) {
-        tab[i] = tab[i+1];
+void fusion(int fusion[], int tab1[], int tab2[], int n1, int n2) {
+    for (int i = 0; i < n1; i++) {
+        fusion[i] = tab1[i];
     }
-
-    tab[n - 1] = temp;
-
-    printf("Tableau après rotation : ");
-    for (int i = 0; i < n; i++) {
-        printf("%d ", tab[i]);
+    for (int j = 0; j < n2; j++) {
+        fusion[n1 + j] = tab2[j];
     }
+}
+
+int main(void) {
+    int nb1, nb2;
+
+    // Lire les tailles
+    scanf("%d", &nb1);
+    int tab1[nb1];
+
+    recup_valeur(tab1, nb1);
+
+    scanf("%d", &nb2);
+    int tab2[nb2];
+
+    recup_valeur(tab2, nb2);
+
+    int nb3 = nb1 + nb2;
+    int tab3[nb3];
+
+    // Fusionner
+    fusion(tab3, tab1, tab2, nb1, nb2);
+
+    // Afficher le tableau fusionné
+    printf("Tableau fusionné : ");
+    for (int i = 0; i < nb3; i++) {
+        printf("%d ", tab3[i]);
+    }
+    printf("\n");
 
     return 0;
 }

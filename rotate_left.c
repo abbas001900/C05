f0a757e3633958d0e55477e75065e60a0352735b
Abbas Ismael <aismoctar@gmail.com>
2025-05-29 18:27:29 +0100

    Create rotate_left.c

---
+++ rotate_left.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void) {
+    int n;
+
+    scanf("%d", &n);
+
+    int tab[n];
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &tab[i]);
+    }
+
+    int temp = tab[0];
+
+    for (int i = 0; i < n - 1; i++) {
+        tab[i] = tab[i+1];
+    }
+
+    tab[n - 1] = temp;
+
+    printf("Tableau après rotation : ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", tab[i]);
+    }
+
+    return 0;
+}

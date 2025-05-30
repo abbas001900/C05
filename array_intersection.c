163c0ba4b3ce7ca46ae827f2cd9f76c1bf24e0fb
Abbas Ismael <aismoctar@gmail.com>
2025-05-30 01:17:05 +0100

    Update array_intersection.c

--- array_intersection.c
+++ array_intersection.c
@@ -1,40 +1,36 @@
 #include <stdio.h>
 
 
-int main() {
-    int n1;
-
-
-    scanf("%d", &n1);
-
-    int tab1[n1];
-
-    for (int i = 0; i < n1; i++) {
-        scanf("%d", &tab1[i]);
-    }
-
-
-    int n2;
-
-    scanf("%d", &n2);
-
-    int tab2[n2];
+int main(void) {
     
-    for (int i = 0; i < n2; i++) {
+    int nbre1;
+
+
+    scanf("%d", &nbre1);
+
+    int tab1[nbre1];
+
+    for (int i = 0; i < nbre1; i++)
+        scanf("%d", &tab1[i]);
+
+
+    int nbre2;
+
+    scanf("%d", &nbre2);
+
+    int tab2[nbre2];
+    
+    for (int i = 0; i < nbre2; i++)
         scanf("%d", &tab2[i]);
-    }
 
 
     // Affichage du résultat
     printf("Intersection : ");
-    
-    for (int i = 0; i < n1; i++) {
-        for (int j = 0; j < n2; j++) { 
-            if (tab1[i] == tab2[j]) {
+
+    for (int i = 0; i < nbre1; i++)
+        for (int j = 0; j < nbre2; j++) 
+            if (tab1[i] == tab2[j])
                 printf("%d ", tab1[i]);
-            }
-        }
-    }
 
     printf("\n");
 
